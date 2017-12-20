4bf977119c84d5ef42566879798e3fa5c6e28654
Pixeldenker <bram@pixeldenker.nl>
2017-12-20 14:34:00 +0100

    Added weight faker + correct color to tabbar

--- Repress/Repress/GewichtViewController.swift
+++ Repress/Repress/GewichtViewController.swift
@@ -10,12 +10,35 @@ import Foundation
 import UIKit
 
 class GewichtViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    let weightFaker = WeightFaker.init()
+    
+    @IBOutlet weak var weightLabel: LargeBlueLabel!
+    
+    @IBOutlet weak var leftShoePercentageLabel: SmallBlueLabel!
+    @IBOutlet weak var leftShoeWeightLabel: SmallBlueLabel!
+    
+    @IBOutlet weak var rightShoeWeightLabel: SmallBlueLabel!
+    @IBOutlet weak var rightShoePercentageLabel: SmallBlueLabel!
+    
+    @IBAction func demoButton(_ sender: Any) {
+        let weight: Double = weightFaker.getRandomWeight()
+        self.weightLabel.text = String(weight)
+        displayWeightDistrubution(weight: weight)
+    }
+    
+    func displayWeightDistrubution(weight: Double) {
+        let leftShoeWeight = weightFaker.devideWeight(weight: weight)
+        
+        self.leftShoeWeightLabel.text = String(leftShoeWeight) + "KG"
+        self.rightShoeWeightLabel.text = String(weightFaker.getRightShoeWeight(totalWeight: weight, leftShoeWeight: leftShoeWeight)) + "KG"
     }
     
     @IBAction func button1(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
--- Repress/Repress/Styles/LabelStyles.swift
+++ Repress/Repress/Styles/LabelStyles.swift
@@ -13,21 +13,27 @@ class LabelStyles {
     static func applyLabelStyles() {
         let smallBlueLabel = SmallBlueLabel.appearance()
         smallBlueLabel.textColor = repressBlue
-        smallBlueLabel.font = UIFont(name: "AvenirNext-DemiBold", size: 14.0)
+        smallBlueLabel.font = UIFont(name: "AvenirNext-Bold", size: 14.0)
         smallBlueLabel.textAlignment = .center
         
         let largeBlueLabel = LargeBlueLabel.appearance()
         largeBlueLabel.textColor = repressBlue
-        largeBlueLabel.font = UIFont(name: "AvenirNext-DemiBold", size: 32.0)
+        largeBlueLabel.font = UIFont(name: "AvenirNext-Bold", size: 32.0)
         largeBlueLabel.textAlignment = .right
         
         let orangeLabel = OrangeLabel.appearance()
         orangeLabel.textColor = repressOrange
-        orangeLabel.font = UIFont(name: "AvenirNext-DemiBold", size: 16.0)
+        orangeLabel.font = UIFont(name: "AvenirNext-Bold", size: 16.0)
         orangeLabel.textAlignment = .left
+        
+        let titleLabel = TitleLabel.appearance()
+        titleLabel.textColor = repressBlue
+        titleLabel.font = UIFont(name: "AvenirNext-Bold", size: 24.0)
+        titleLabel.textAlignment = .center
     }
 }
 
 class SmallBlueLabel: UILabel {}
 class LargeBlueLabel: UILabel {}
 class OrangeLabel: UILabel {}
+class TitleLabel: UILabel {}
